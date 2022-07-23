7f2ea6223a86cc15cefdd4f7961f1b78e16c24ee
takak2166 <28010438+tk9559@users.noreply.github.com>
2022-07-23 06:45:07 +0000

    gen id

--- isuports.go
+++ isuports.go
@@ -47,6 +47,8 @@ var (
 	adminDB *sqlx.DB
 
 	sqliteDriverName = "sqlite3"
+
+	GlobalID int64 = 0
 )
 
 // 環境変数を取得する、なければデフォルト値を返す
@@ -98,25 +100,13 @@ func createTenantDB(id int64) error {
 }
 
 // システム全体で一意なIDを生成する
-func dispenseID(ctx context.Context) (string, error) {
+func dispenseID() (string, error) {
 	var id int64
 	var lastErr error
-	for i := 0; i < 10; i++ {
-		var ret sql.Result
-		ret, err := adminDB.ExecContext(ctx, "REPLACE INTO id_generator (stub) VALUES (?);", "a")
-		if err != nil {
-			if merr, ok := err.(*mysql.MySQLError); ok && merr.Number == 1213 { // deadlock
-				lastErr = fmt.Errorf("error REPLACE INTO id_generator: %w", err)
-				continue
-			}
-			return "", fmt.Errorf("error REPLACE INTO id_generator: %w", err)
-		}
-		id, err = ret.LastInsertId()
-		if err != nil {
-			return "", fmt.Errorf("error ret.LastInsertId: %w", err)
-		}
-		break
-	}
+
+	GlobalID += 1
+	id = GlobalID
+
 	if id != 0 {
 		return fmt.Sprintf("%x", id), nil
 	}
@@ -793,7 +783,7 @@ func playersAddHandler(c echo.Context) error {
 
 	pds := make([]PlayerDetail, 0, len(displayNames))
 	for _, displayName := range displayNames {
-		id, err := dispenseID(ctx)
+		id, err := dispenseID()
 		if err != nil {
 			return fmt.Errorf("error dispenseID: %w", err)
 		}
@@ -911,7 +901,7 @@ func competitionsAddHandler(c echo.Context) error {
 	title := c.FormValue("title")
 
 	now := time.Now().Unix()
-	id, err := dispenseID(ctx)
+	id, err := dispenseID()
 	if err != nil {
 		return fmt.Errorf("error dispenseID: %w", err)
 	}
@@ -1081,7 +1071,7 @@ func competitionScoreHandler(c echo.Context) error {
 				fmt.Sprintf("error strconv.ParseUint: scoreStr=%s, %s", scoreStr, err),
 			)
 		}
-		id, err := dispenseID(ctx)
+		id, err := dispenseID()
 		if err != nil {
 			return fmt.Errorf("error dispenseID: %w", err)
 		}
